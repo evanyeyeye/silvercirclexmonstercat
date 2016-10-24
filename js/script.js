fc3d37c5e340473f28535edbbbf66fa45b3337c4
William Zhang <willzhang05@gmail.com>
2016-10-24 14:17:29 -0400

    Add some stuff

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 document.getElementById("vote-button").addEventListener("click", function() {
-    var popup = document.createElement("div");
+    var popup = document.getElementById("popup");
     var iframe = document.createElement("iframe");
+    var wrapper = document.getElementById("wrapper");
+    popup.style.opacity = 1;
+    popup.style.display = "block";
 })
