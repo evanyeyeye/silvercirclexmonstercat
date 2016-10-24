4b7791db2671043f50247aa47f635ed17b486647
William Zhang <willzhang05@gmail.com>
2016-10-24 18:55:15 -0400

    Tried to fix stupid ios click issue

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
 var wrapper = document.getElementById("wrapper");
-document.getElementById("signup-button").addEventListener("click", function() {
-    var popup = document.getElementById("popup");
-    popup.style.opacity = 1;
-    popup.style.visibility = "visible";
-    var iframe = popup.children[1];
-    iframe.src = "https://splice.com/accounts/sign_up";
-    iframe.width = "500px";
-    iframe.height = "500px";
-})
-document.getElementById("vote-button").addEventListener("click", function() {
-    var popup = document.getElementById("popup");
-    popup.style.opacity = 1;
-    popup.style.visibility = "visible";
-    var iframe = popup.children[1];
-    iframe.src = "https://splice.com/accounts/sign_up";
-    iframe.width = "500px";
-    iframe.height = "500px";
-})
+document.getElementById("signup-button").addEventListener("click", function() { togglePopup("https://splice.com/accounts/sign_up") });
+document.getElementById("vote-button").addEventListener("click", function() { togglePopup("https://splice.com/accounts/sign_up") });
 
-document.getElementById("exit-button").addEventListener("click", function() {
+function togglePopup(url) {
+    alert("aasdf");
+    var popup = document.getElementById("popup");
+    popup.style.opacity = 1;
+    popup.style.visibility = "visible";
+    var iframe = popup.children[1];
+    iframe.src = url;
+    iframe.width = "500px";
+    iframe.height = "500px";
+}
+
+document.getElementById("exit-button").onclick = function() {
     popup.style.opacity = 0;
     popup.style.visibility = "hidden";
-})
+}
 
