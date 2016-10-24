0ed10bb126b72d2080ba4dfa6e9b090de68aba72
William Zhang <willzhang05@gmail.com>
2016-10-24 17:15:16 -0400

    Add fonts and scripts and stuff

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
+var wrapper = document.getElementById("wrapper");
 document.getElementById("signup-button").addEventListener("click", function() {
     var popup = document.getElementById("popup");
-    var iframe = document.createElement("iframe");
-    var wrapper = document.getElementById("wrapper");
     popup.style.opacity = 1;
-    popup.style.display = "block";
+    popup.style.visibility = "visible";
+    var iframe = popup.children[1];
+    iframe.src = "https://splice.com/accounts/sign_up";
+    iframe.width = "500px";
+    iframe.height = "500px";
 })
 document.getElementById("vote-button").addEventListener("click", function() {
     var popup = document.getElementById("popup");
-    var iframe = document.createElement("iframe");
-    var wrapper = document.getElementById("wrapper");
     popup.style.opacity = 1;
-    popup.style.display = "block";
+    popup.style.visibility = "visible";
+    var iframe = popup.children[1];
+    iframe.src = "https://splice.com/accounts/sign_up";
+    iframe.width = "500px";
+    iframe.height = "500px";
 })
+
+document.getElementById("exit-button").addEventListener("click", function() {
+    popup.style.opacity = 0;
+    popup.style.visibility = "hidden";
+})
+
