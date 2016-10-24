b9036bd5f34903551f5bb7e3fb8877ca28088b82
William Zhang <willzhang05@gmail.com>
2016-10-24 16:19:05 -0400

    Added iframe stuff

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+document.getElementById("signup-button").addEventListener("click", function() {
+    var popup = document.getElementById("popup");
+    var iframe = document.createElement("iframe");
+    var wrapper = document.getElementById("wrapper");
+    popup.style.opacity = 1;
+    popup.style.display = "block";
+})
 document.getElementById("vote-button").addEventListener("click", function() {
     var popup = document.getElementById("popup");
     var iframe = document.createElement("iframe");
