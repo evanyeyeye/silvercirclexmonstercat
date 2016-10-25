f14318318766a7dd633874a5b0d3b9e88a91a983
AnubisSQLi <watalanubhava@gmail.com>
2016-10-24 22:51:47 -0400

    Anubis' changes to workflow and making it heroku pushable

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 var wrapper = document.getElementById("wrapper");
 document.onclick = function() {
-    
     document.getElementById("signup-button").onclick = function() {};
-    document.getElementById("vote-button").onclick = function() {};
+    document.getElementById("next-button").onclick = function() {
+        this.remove();
+        document.getElementById("bar-title").innerText = "2. Like my track";
+    };
 }
 document.getElementById("signup-button").addEventListener("click", function() { togglePopup("https://splice.com/accounts/sign_up") });
 document.getElementById("signup-button").addEventListener("touchstart", function() { togglePopup("https://splice.com/accounts/sign_up") });
-document.getElementById("vote-button").addEventListener("click", function() { togglePopup("https://splice.com/accounts/sign_up") });
-document.getElementById("vote-button").addEventListener("touchstart", function() { togglePopup("https://splice.com/accounts/sign_up") });
+document.getElementById("next-button").addEventListener("click", function() { togglePopup("https://splice.com/SilverCircle/mcatmix2016") });
+document.getElementById("next-button").addEventListener("touchstart", function() { togglePopup("https://splice.com/SilverCircle/mcatmix2016") });
 
 function togglePopup(url) {
     var popup = document.getElementById("popup");
+    document.getElementById("bar-title")
     popup.style.opacity = 1;
     popup.style.visibility = "visible";
     var iframe = popup.children[1];
@@ -23,4 +26,3 @@ document.getElementById("exit-button").onclick = function() {
     popup.style.opacity = 0;
     popup.style.visibility = "hidden";
 }
-
